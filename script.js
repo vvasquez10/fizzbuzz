bd3033e3ed31522237b38d64831810f9ec43ef90
Victor Vasquez <daxos10.0@gmail.com>
2022-01-06 12:59:30 -0500

    correcion2

--- script.js
+++ script.js
@@ -1,42 +1,16 @@
-function printPairs(){
-    for(var i=1; i<=20; i++){
-        if(i%2!=0){
-            console.log(i)
+for(var i=1; i<=100; i++){
+    if(i%3==0){
+        if(i%5==0){
+            console.log("FizzBuzz")
+        }
+        else{
+            console.log("Fizz")
         }
     }
-}
-
-function decreaseMul(){
-    for(var i=100; i>=0; i--){
-        if(i%3==0){
-            console.log(i)
-        }
+    else if(i%5==0){
+        console.log("Buzz")
     }
-}
-
-function printSec(times){ //cuantos numeros de la secuencia desea, partiendo desde el +4
-    var i=4;
-    for(var x=0; x<times; x++){
+    else{
         console.log(i)
-        i=i-1.5
     }
 }
-
-function sigma(){
-    var sum=0;
-    for(var i=1; i<=100; i++){
-        sum=sum+i
-    }
-    console.log(sum)
-}
-
-function factorial(){
-    var product=1;
-    for(var i=1; i<=12; i++){
-        product=product*i
-    }
-    console.log(product)
-}
-
-factorial()
-
